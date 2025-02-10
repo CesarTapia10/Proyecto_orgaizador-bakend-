5b47ca7a8ddee0f39b9642409042b8d7f6de9d49
CesarTapia10 <u202210918@upc.edu.pe>
2025-02-10 15:44:06 -0500

    cambios para el funcionamiento del render

--- src/main/java/pe/edu/upc/proyectoverano/securities/WebSecurityConfig.java
+++ src/main/java/pe/edu/upc/proyectoverano/securities/WebSecurityConfig.java
@@ -79,11 +79,11 @@ public class WebSecurityConfig {
         return httpSecurity.build();
     }
 
-   /* private static final String[] AUTH_WHITELIST = {
+   private static final String[] AUTH_WHITELIST = {
             "/api/v1/auth/**",
             "/v3/api-docs/**",
             "/v3/api-docs.yaml",
             "/swagger-ui/**",
             "/swagger-ui.html"
-    };*/
+    };
 }
